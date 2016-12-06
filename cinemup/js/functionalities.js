0cede5c7c7d3d6db4a4970d77d317b0cb2579d3a
DESKTOP-U6T94J6\Usuario <alexcordonvila@gmail.com>
2016-12-06 15:07:48 +0100

    Grid menu fixed + focus at first element

--- cinemup/js/functionalities.js
+++ cinemup/js/functionalities.js
@@ -7,6 +7,157 @@ var API_KEY = "e23c818ec74b3447e740a6d758f88ddc";
 var UPDATE_TIME_CLOCK = 60000;
 var petition_parameter = "popular";
 
+window.onload = function() {
+
+	updateClock();
+	setInterval(updateClock, UPDATE_TIME_CLOCK);
+
+	$(".menu-panel").hide()	//Amaguèm el carrousel de pelicules i el panell d'informació per mostrar la galeria inicial
+	$(".info-panel").hide()
+
+	if(!showingFilms && !showingDescription){
+		$('.options').children().css( 'border', '1px solid black'); 
+		$('.options').children().first().css( 'border', '1px solid white');     //Fem focus al primer element al iniciar l'aplicació
+		$.caph.focus.activate(function(nearestFocusableFinderProvider, controllerProvider) {
+	        controllerProvider.onFocused(function(event, originalEvent) {
+	        	$(event.currentTarget).css( 'border', '1px solid white');
+	        	
+	        });
+	        controllerProvider.onBlurred(function(event, originalEvent) {
+	        	$(event.currentTarget).css( 'border', '1px solid transparent');
+	        });
+	        controllerProvider.onSelected(function(event, originalEvent){
+	        	var petition = $(event.currentTarget).attr('data-petitiontype');
+	 	 		var fill_info = $(event.currentTarget).children("h1").html();
+	 	 		$('#petition-type').html(fill_info);
+	 	 		console.log("STARTING HTTP MOVIE MENU...");
+	 	 		getHttpRequestMenu("https://api.themoviedb.org/3/movie/" + petition + "?api_key=" + API_KEY + "&language=" +LANGUAGE_RESPONSE, getJSONDescription);
+				
+			});
+	    });
+	}
+	 
+	keyController(); //Escoltem els events de teclats
+
+	
+};
+
+
+function keyController(){
+document.addEventListener('keydown', function (e) {
+    	
+    	var e = e || e.keyCode;
+
+    	if(e.keyCode == TvKeyCode.KEY_BACK){
+    		console.log("HELLOOOOO");
+    	}
+    	
+    	if (e.keyCode == '13') {
+    		console.log("info - key ENTER pressed");
+    		if(showingFilms){
+    			changeWindow(2);
+    		}
+    		if(ShowingDescription){
+    			
+    			
+    		}
+    	}
+    	if (e.keyCode == TvKeyCode.KEY_INFO) { 
+    		console.log("info - key ESC pressed");
+    		if (showingFilms) { changeWindow(4)};
+    		if (showingDescription) { changeWindow(3)};
+    	}    	
+    	if (e.keyCode == '37') { 
+    		console.log("info - key LEFT pressed");
+    		if(showingFilms){
+				$(".film-list").slick('slickPrev');
+				updateFocus();
+    		}
+    	}
+    	if (e.keyCode == '38') { 
+    		console.log("info - key UP pressed");	
+    	}
+    	if (e.keyCode == '39') { 
+    		console.log("info - key RIGHT pressed");
+    		if(showingFilms){
+	    		$(".film-list").slick('slickNext');
+	    		updateFocus();	
+    		}
+    	}
+    	if (e.keyCode == '40') { 
+    		console.log("info - key DOWN pressed");
+    	}    	    	    	
+	});
+	
+	
+}
+function changeWindow(option){
+	
+	switch(option){
+		case 1:
+			$(".selector-panel").fadeOut(FADE_TIME);
+			$(".menu-panel").fadeIn(FADE_TIME);
+			showingFilms = true;
+			showingDescription = false;
+		break;
+		case 2:
+			$(".menu-panel").fadeOut(FADE_TIME);
+			$(".time-panel").fadeOut(FADE_TIME);
+			$(".info-panel").fadeIn(FADE_TIME);
+			showingDescription = true;
+			showingFilms = false;
+		break;
+		case 3:
+			$(".info-panel").fadeOut(FADE_TIME);
+			$(".time-panel").fadeIn(FADE_TIME);
+			$(".menu-panel").fadeIn(FADE_TIME);
+			showingDescription = false;
+			showingFilms = true;	
+			
+		break;
+		case 4:
+			$('.film-list').slick('unslick');	
+			initializeSlick();
+			updateFocus();
+			$(".menu-panel").fadeOut(FADE_TIME);
+			$(".selector-panel").fadeIn(FADE_TIME);
+			showingDescription = false;
+			showingFilms = false;
+		break;		
+	}
+
+}
+function updateFocus(){
+	
+	//Change focus class
+	$('.active').removeClass('active');
+	$('.slick-center .focus').addClass('active');
+
+	//Update information text (title and rating)
+	var title = $(".active").attr('data-title');
+	var rating = $(".active").attr('data-vote');
+	var starsFilled = Math.round(rating * 5 / 10);
+	var starsEmpty = 5 - starsFilled;
+	var starsRating = '<i class="fa fa-star" aria-hidden="true"></i>'.repeat(starsFilled) + '<i class="fa fa-star-o" aria-hidden="true"></i>'.repeat(starsEmpty) + "  " + rating;
+	$(".title-name").html(title);
+	$(".title-rating").html(starsRating);
+
+	//Fill image background
+	var path_image = $(".active").attr('data-backgroundimage');
+	$('.background-photo').fadeOut('slow',function(){
+        $(this).attr('src',path_image).fadeIn(FADE_TIME, 'swing');
+    });
+
+	//Fill info about focus film
+	console.log("STARTING HTTP MOVIE DESCRIPTION...");
+	var id = $(".active").attr('data-id');
+	getHttpRequestDescription("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + API_KEY + "&language=" + LANGUAGE_RESPONSE, getJSONMenu);
+//	$.caph.focus.init(function(nearestFocusableFinderProvider, controllerProvider) {
+//		controllerProvider.onFocused(function(event, originalEvent) {
+//        	$(event.btn).css( 'border', '1px solid white');
+//		});
+//    });
+}
 function getHttpRequestMenu(theUrl, callback) {
 
     var xmlHttp = new XMLHttpRequest();
@@ -44,7 +195,7 @@ function getJSONMenu(json){
 	$(".focus").first().addClass("active");
 	
 	//Refresh text to match info
-	changeWindow(1);
+	changeWindow(1); //Passèm de finestra 1 a 2
 	initializeSlick();
 	updateFocus();
 	
@@ -78,43 +229,6 @@ function getJSONDescription(json){
 	console.log("...FINISHED HTTP MOVIE DESCRIPTION");
 
 }
-
-function changeWindow(option){
-	
-	switch(option){
-		case 1:
-			$(".selector-panel").fadeOut(FADE_TIME);
-			$(".menu-panel").fadeIn(FADE_TIME);
-			showingFilms = true;
-			showingDescription = false;
-		break;
-		case 2:
-			$(".menu-panel").fadeOut(FADE_TIME);
-			$(".time-panel").fadeOut(FADE_TIME);
-			$(".info-panel").fadeIn(FADE_TIME);
-			showingDescription = true;
-			showingFilms = false;
-		break;
-		case 3:
-			$(".info-panel").fadeOut(FADE_TIME);
-			$(".time-panel").fadeIn(FADE_TIME);
-			$(".menu-panel").fadeIn(FADE_TIME);
-			showingDescription = false;
-			showingFilms = true;	
-		break;
-		case 4:
-			$('.film-list').slick('unslick');	
-			initializeSlick();
-			updateFocus();
-			$(".menu-panel").fadeOut(FADE_TIME);
-			$(".selector-panel").fadeIn(FADE_TIME);
-			showingDescription = false;
-			showingFilms = false;
-		break;		
-	}
-
-}
-
 function updateClock(){
 	
 	var date = new Date()
@@ -129,104 +243,7 @@ function updateClock(){
 
 }
 
-function updateFocus(){
-	
-	//Change focus class
-	$('.active').removeClass('active');
-	$('.slick-center .focus').addClass('active');
 
-	//Update information text (title and rating)
-	var title = $(".active").attr('data-title');
-	var rating = $(".active").attr('data-vote');
-	var starsFilled = Math.round(rating * 5 / 10);
-	var starsEmpty = 5 - starsFilled;
-	var starsRating = '<i class="fa fa-star" aria-hidden="true"></i>'.repeat(starsFilled) + '<i class="fa fa-star-o" aria-hidden="true"></i>'.repeat(starsEmpty) + "  " + rating;
-	$(".title-name").html(title);
-	$(".title-rating").html(starsRating);
-
-	//Fill image background
-	var path_image = $(".active").attr('data-backgroundimage');
-	$('.background-photo').fadeOut('slow',function(){
-        $(this).attr('src',path_image).fadeIn(FADE_TIME, 'swing');
-    });
-
-	//Fill info about focus film
-	console.log("STARTING HTTP MOVIE DESCRIPTION...");
-	var id = $(".active").attr('data-id');
-	getHttpRequestDescription("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + API_KEY + "&language=" + LANGUAGE_RESPONSE, getJSONMenu);
-
-}
-
-window.onload = function() {
-
-	updateClock();
-	setInterval(updateClock, UPDATE_TIME_CLOCK);
-
-	$(".menu-panel").hide()
-	$(".info-panel").hide()
-
-	if(!showingFilms && !showingDescription){
-		$.caph.focus.activate(function(nearestFocusableFinderProvider, controllerProvider) {
-	        controllerProvider.onFocused(function(event, originalEvent) {
-	        	$(event.currentTarget).css( 'border', '1px solid white');
-	        });
-	        controllerProvider.onBlurred(function(event, originalEvent) {
-	        	$(event.currentTarget).css( 'border', '1px solid transparent');
-	        });
-	        controllerProvider.onSelected(function(event, originalEvent){
-	        	var petition = $(event.currentTarget).attr('data-petitiontype');
-	 	 		var fill_info = $(event.currentTarget).children("h1").html();
-	 	 		$('#petition-type').html(fill_info);
-	 	 		console.log("STARTING HTTP MOVIE MENU...");
-	 	 		getHttpRequestMenu("https://api.themoviedb.org/3/movie/" + petition + "?api_key=" + API_KEY + "&language=" +LANGUAGE_RESPONSE, getJSONDescription);
-				
-			});
-	    });
-	}
-	
-
-	document.addEventListener('keydown', function (e) {
-    	
-    	var e = e || e.keyCode;
-
-    	if(e.keyCode == TvKeyCode.KEY_BACK){
-    		console.log("HELOOOOO");
-    	}
-    	
-    	if (e.keyCode == '13') {
-    		console.log("info - key ENTER pressed");
-    		if(showingFilms){
-    			changeWindow(2);
-    		}
-    	}
-    	if (e.keyCode == TvKeyCode.KEY_INFO) { 
-    		console.log("info - key ESC pressed");
-    		if (showingFilms) { changeWindow(4)};
-    		if (showingDescription) { changeWindow(3)};
-    	}    	
-    	if (e.keyCode == '37') { 
-    		console.log("info - key LEFT pressed");
-    		if(showingFilms){
-				$(".film-list").slick('slickPrev');
-				updateFocus();
-    		}
-    	}
-    	if (e.keyCode == '38') { 
-    		console.log("info - key UP pressed");	
-    	}
-    	if (e.keyCode == '39') { 
-    		console.log("info - key RIGHT pressed");
-    		if(showingFilms){
-	    		$(".film-list").slick('slickNext');
-	    		updateFocus();	
-    		}
-    	}
-    	if (e.keyCode == '40') { 
-    		console.log("info - key DOWN pressed");
-    	}    	    	    	
-	});
-
-};
 
 function initializeSlick(){	
 
